7224fa6a4dacdef9d6deb37a01efe7a0b3fce971
Chirag2010 <87812804+Chirag2010@users.noreply.github.com>
2022-02-26 19:15:18 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,22 @@
+status="";
+function preload(){
+    img=loadImage("desk.jpg");
+}
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+    }
+    function modelLoaded() {
+        console.log("Model Loaded!")
+        status=true;
+        objectDetector.detect(img,gotResult);
+    }
+    function gotResult(results,error){
+        if(error){
+      console.log(error);
+  }
+        else{
+     console.log(results);
+}}
